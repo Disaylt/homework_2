f6687b9b8e57718fa0ef66f8702d063098da9c22
disaylt <disofban@gmail.com>
2022-11-28 16:02:00 +0300

    feat: create friend db service

---
+++ homework_2/Services/Friends/FriendDbService.cs
@@ -0,0 +1,56 @@
+﻿using homework_2.Database;
+using homework_2.Models;
+
+namespace homework_2.Services.Friends
+{
+    public class FriendDbService : IFriendsService
+    {
+        private readonly FriendContext _friendContext;
+        public FriendDbService(FriendContext friendContext)
+        {
+            _friendContext = friendContext;
+        }
+
+        public FriendModel Add(FriendModel friend)
+        {
+            _friendContext.Friends.Add(friend);
+            _friendContext.SaveChanges();
+
+            return friend;
+        }
+
+        public bool Delete(int id)
+        {
+            FriendModel? friend = Get(id);
+
+            if(friend == null) return false;
+
+            _friendContext.Friends.Remove(friend);
+            _friendContext.SaveChanges();
+
+            return true;
+        }
+
+        public FriendModel? Get(int id)
+        {
+            return _friendContext.Friends
+                .FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<FriendModel> GetAll()
+        {
+            IEnumerable<FriendModel> friends = _friendContext.Friends
+                .ToList();
+
+            return friends;
+        }
+
+        public bool Update(FriendModel friend)
+        {
+            _friendContext.Friends.Update(friend);
+            _friendContext.SaveChanges();
+
+            return true;
+        }
+    }
+}
