bd767ece63ecef6e4420be36adccc8b5d9b289b7
disaylt <disofban@gmail.com>
2022-11-28 15:49:28 +0300

    feat: create db context factory for migration

---
+++ homework_2/Database/FriendContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace homework_2.Database
+{
+    public class FriendContextFactory : IDesignTimeDbContextFactory<FriendContext>
+    {
+        public FriendContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<FriendContext>();
+
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+            IConfigurationRoot config = builder.Build();
+
+            string? connectionString = config.GetConnectionString("FriendDb");
+            optionsBuilder.UseSqlServer(connectionString);
+            return new FriendContext(optionsBuilder.Options);
+        }
+    }
+}
